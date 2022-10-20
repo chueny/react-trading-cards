980b062b3f4111c209a229f98317130ff38db490
chueny <chueyangib@gmail.com>
2022-10-20 11:45:07 -0500

    Add two trading cards and fixed skills props
    Co-authored-by: Andrea Martz <14165788+andreamartz@users.noreply.github.com>

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+  <div>
+    <div>Welcome, user! </div>
+    <a href="/cards"> Link to the cards</a>
+    <img src="/static/img/balloonicorn.jpg"/>
+  </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -13,7 +13,7 @@ function TradingCard(props) {
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
@@ -31,4 +31,14 @@ ReactDOM.render(
 ReactDOM.render(
   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
   document.querySelector('#llambda'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Merge" skill="merges files" imgUrl="/static/img/merge.png" />,
+  document.querySelector('#merge'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Seedpy" skill="flying" imgUrl="/static/img/seedpy.jpeg" />,
+  document.querySelector('#seedpy'),
 );
